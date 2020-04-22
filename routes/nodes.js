e48aeac2352efc5d5c3826c811fbd3e5291df052
bbjansen <bobjansen@pm.me>
2020-04-22 10:52:29 +0200

    rely on lto.tools temp

--- routes/nodes.js
+++ routes/nodes.js
@@ -6,17 +6,25 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../utils/utils').knex
+const axios = require('axios')
+
+
+https://lto.tools/nodes/json
 
 // Get address by id
 router.get('/all',
 async function (req, res, next) {
   try {
-    const getNodes = await db('nodes')
-      .leftJoin('status', 'nodes.address', 'status.address')
-      .leftJoin('geo', 'nodes.address', 'geo.address')
-      .select()
 
-    res.json(getNodes)
+
+    // const getNodes = await db('nodes')
+    //  .leftJoin('status', 'nodes.address', 'status.address')
+    //  .leftJoin('geo', 'nodes.address', 'geo.address')
+    //  .select()
+
+    const nodes = await axios.get('https://lto.tools/nodes/json')
+    res.json(nodes.data)
+    
   } catch (err) {
     next(err)
   }
